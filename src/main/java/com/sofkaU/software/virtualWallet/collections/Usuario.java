9b5f688f4c9c70cb0044549c460ea659305e4e58
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-06-24 17:24:02 -0500

    se agregó colección Usuario

---
+++ src/main/java/com/sofkaU/software/virtualWallet/collections/Usuario.java
@@ -0,0 +1,25 @@
+package com.sofkaU.software.virtualWallet.collections;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Usuario")
+public class Usuario {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private String rol;
+    private boolean estaActivo;
+    private boolean correoVerificado;
+
+
+}
