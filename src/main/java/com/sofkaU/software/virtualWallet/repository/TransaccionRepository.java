8dd43fa1af78af27297cadb312ac7b282a6df46d
Willian Franco <ing.wfranco@gmail.com>
2022-06-25 16:01:07 -0500

    Feat Transaccion
    se adiciona EndPoint Transaccion, mapper y repository

--- src/main/java/com/sofkaU/software/virtualWallet/repository/TransaccionRepository.java
+++ src/main/java/com/sofkaU/software/virtualWallet/repository/TransaccionRepository.java
@@ -1,2 +1,13 @@
-package com.sofkaU.software.virtualWallet.repository;public interface TransaccionRepository {
+package com.sofkaU.software.virtualWallet.repository;
+
+import com.sofkaU.software.virtualWallet.collections.Transaccion;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface TransaccionRepository extends ReactiveCrudRepository<Transaccion, String> {
+
+/*    Flux<Transaccion> findallByCorreoOrigen(String correoOrigen);
+    Flux<Transaccion> findallByCorreoDestino(String correoDestino);*/
 }
--- src/main/java/com/sofkaU/software/virtualWallet/routes/TransaccionRoute.java
+++ src/main/java/com/sofkaU/software/virtualWallet/routes/TransaccionRoute.java
@@ -1,2 +1,34 @@
-package com.sofkaU.software.virtualWallet.routes;public class TransaccionRoute {
+package com.sofkaU.software.virtualWallet.routes;
+
+import com.sofkaU.software.virtualWallet.dto.TransaccionDTO;
+import com.sofkaU.software.virtualWallet.useCase.CreateTransaccionUseCase;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+@Configuration
+public class TransaccionRoute {
+
+    @Bean
+/*    annotattion swagger*/
+    public RouterFunction<ServerResponse> createTransaccion(CreateTransaccionUseCase createTransaccionUseCase) {
+        Function<TransaccionDTO, Mono<ServerResponse>> executor = transaccionDTO -> createTransaccionUseCase.apply(transaccionDTO)
+                .flatMap(result -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(result));
+
+        return route(
+                POST("/createTrans").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(TransaccionDTO.class).flatMap(executor)
+        );
+    }
 }
