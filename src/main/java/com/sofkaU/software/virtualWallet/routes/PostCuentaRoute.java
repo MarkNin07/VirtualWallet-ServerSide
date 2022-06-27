efdd6f2ab5684b1068c8d49410999c6c651852d8
Marcos Nino <tmnino@uninorte.edu.co>
2022-06-26 19:14:49 -0500

    Crear cuenta UseCase y Route fueron creadas

--- src/main/java/com/sofkaU/software/virtualWallet/routes/PostCuentaRoute.java
+++ src/main/java/com/sofkaU/software/virtualWallet/routes/PostCuentaRoute.java
@@ -1,26 +1,27 @@
 package com.sofkaU.software.virtualWallet.routes;
 
+
 import com.sofkaU.software.virtualWallet.dto.CuentaDto;
-import com.sofkaU.software.virtualWallet.useCase.UpdateCuentaUseCase;
+import com.sofkaU.software.virtualWallet.useCase.PostCuentaUseCase;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-@Configuration
-public class UpdateCuentaRoute {
+@Service
+public class PostCuentaRoute {
 
     @Bean
-    public RouterFunction<ServerResponse> actualizarCuenta(UpdateCuentaUseCase updCuenta){
-        return route(PUT("/update/cuenta").and(accept(MediaType.APPLICATION_JSON)),
+    public RouterFunction<ServerResponse> crearCuenta(PostCuentaUseCase postCuenta){
+        return route(POST("/create/cuenta").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(CuentaDto.class)
-                        .flatMap(updCuenta::updateCuenta)
-                        .flatMap(cuentaDto -> ServerResponse.status(HttpStatus.ACCEPTED)
+                        .flatMap(postCuenta::crearCuenta)
+                        .flatMap(cuentaDto -> ServerResponse.status(HttpStatus.CREATED)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .bodyValue(cuentaDto))
                         .onErrorResume(e -> ServerResponse.status(HttpStatus.BAD_REQUEST).build()));
